30a8540c56a8074619db2abd5a0bf6de994bb63a
Ngo Xuan Hai <hngoxuantg@gmail.com>
2025-05-17 11:33:52 +0700

    Initial commit

---
+++ QuanLyCuaHangChoThueXeMay/QuanLyCuaHangChoThueXeMay/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuanLyCuaHangChoThueXeMay.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
