b4e5a336ce2f416ffa6fb255ea11827dd25dbe1b
Ngô Xuân Hải <23dh110913@st.huflit.edu.vn>
2025-05-19 17:10:01 +0700

    Delete QuanLyCuaHangChoThueXeMay directory

--- QuanLyCuaHangChoThueXeMay/QuanLyCuaHangChoThueXeMay/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace QuanLyCuaHangChoThueXeMay.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
